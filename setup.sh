fbf6f534dfe3f5e5366a24e99d99ff7fbbf921d2
Rayan Alabbad <rayan@rayanabbad.com>
2025-06-16 16:09:37 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 nix-shell -p git
-cd
+cd /home/rayanalabbad
 git clone https://github.com/rayanalabbad/my-dotfiles.git
-cd my-dotfiles
-mkdir -p ~/Pictures
-cp -r ~/my-dotfiles/Pictures/. ~/Pictures/
+cd /home/rayanalabbad/my-dotfiles
+mkdir -p /home/rayanalabbad/Pictures
+cp -r /home/rayanalabbad/my-dotfiles/Pictures/. /home/rayanalabbad/Pictures/
+cp -r /home/rayanalabbad/my-dotfiles/etc/nixos /etc/
+mkdir -p /home/rayanalabbad/.config/nvim
+cp -r /home/rayanalabbad/my-dotfiles/.config/nvim /home/rayanalabbad/.config/
